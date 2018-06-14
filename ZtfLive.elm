f2fd2b7853bcedd654bb99985fec69df9e124921
Colin Slater <ctslater@uw.edu>
2018-06-14 15:51:45 -0700

    Minor cleanup.

--- ZtfLive.elm
+++ ZtfLive.elm
@@ -5,15 +5,14 @@ import Html.Events exposing (onClick)
 import List
 import Http
 import Json.Decode
+import Time exposing (Time)
 
 import AltAz exposing (altAzPlot, PolarSpec, makePolarMapping)
-import Coordinates exposing (RaDec, AltAz)
 
 main =
   Html.program
     {
     init = init ,
---    model = model,
     view = view,
     update = update,
     subscriptions = subscriptions
@@ -24,29 +23,22 @@ main =
 init: (Model, Cmd Msg)
 init =
   (
-  Model 0.0 [],
+  Model 0.50 [],
   sendGetFieldReq)
 
 -- MODEL
 
 type alias Model = { lst:  Float, fields: List(Field) }
 
-type alias Field = { ra: Float, dec: Float, alerts: String}
-
---model : Model
---model =
---  { lst = 0.0,
---    fields = [ RaDec 3.0 32.0,
---               RaDec 5.0 44.0] }
-
+type alias Field = { ra: Float, dec: Float, alerts: String, visit: String}
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Sub.none
+  Time.every Time.minute Tick
 
 -- UPDATE
 
-type Msg = Reset | IncLST | DecLST
+type Msg = Reset | IncLST | DecLST | Tick Time
           | GetFields | VisitsUpdate (Result Http.Error (List Field))
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -55,20 +47,16 @@ update msg model =
     Reset -> (model, Cmd.none)
     IncLST -> ({ model | lst = model.lst + ((2 * pi)/24) }, Cmd.none)
     DecLST -> ({ model | lst = model.lst - ((2 * pi)/24) }, Cmd.none)
+    Tick _ -> (model, sendGetFieldReq)
     GetFields -> (model, sendGetFieldReq)
     VisitsUpdate (Ok fields) -> ({ model | fields = fields}, Cmd.none)
     VisitsUpdate (Err _) -> (model, Cmd.none)
 
 fieldDecoder: Json.Decode.Decoder Field
-fieldDecoder = Json.Decode.map3 Field (Json.Decode.field "RA" Json.Decode.float)
+fieldDecoder = Json.Decode.map4 Field (Json.Decode.field "RA" Json.Decode.float)
                                       (Json.Decode.field "Dec" Json.Decode.float)
                                       (Json.Decode.field "alertcount" Json.Decode.string)
-
-addFields: String -> List Field
-addFields dataString = 
-  case Json.Decode.decodeString (Json.Decode.list fieldDecoder) dataString of
-  Err _ -> []
-  Ok newFields -> newFields
+                                      (Json.Decode.field "visit" Json.Decode.string)
 
 sendGetFieldReq: Cmd Msg
 sendGetFieldReq =
@@ -77,11 +65,6 @@ sendGetFieldReq =
     request = Http.get url (Json.Decode.list fieldDecoder)
   in
     Http.send VisitsUpdate request
-
--- {"Dec": 40.55, "RA": 243.95089, "alertcount": "91", "field": "722",
--- "firstseen": "2018-06-13T16:04:53.464187", "lastseen":
--- "2018-06-13T16:04:56.299064", "visit": "20180612261030"}
-
 
 -- VIEW
 
@@ -96,15 +79,15 @@ view model =
                                 ("height", "100vh"), ("padding", "1em") ] ] [
       altAzPlot model.fields model.lst mapping,
       lstControls model,
-      Html.button [ onClick GetFields ] [ text "Get Fields" ],
+      -- Html.button [ onClick GetFields ] [ text "Get Fields" ],
       alertsTable model
     ]
 
 alertsTableRow: Field -> Html msg
 alertsTableRow field =
     Html.tr [] [
-      Html.td [] [text "1032432"],
-      Html.td [] [text "01:03"],
+      Html.td [] [text field.visit],
+      -- Html.td [] [text "01:03"],
       Html.td [] [text (toString field.ra)],
       Html.td [] [text (toString field.dec)],
       Html.td [] [text field.alerts]
@@ -114,10 +97,10 @@ alertsTableRow field =
 alertsTable : Model -> Html Msg
 alertsTable model =
   Html.div [] [
-    Html.table [] ([
+    Html.table [HtmlAttr.style [("border-spacing", "0.5em")]] ([
       Html.thead [] [
         Html.th [] [text "Visit"],
-        Html.th [] [text "Time (UTC)"],
+        -- Html.th [] [text "Time (UTC)"],
         Html.th [] [text "Ra"],
         Html.th [] [text "Dec"],
         Html.th [] [text "# of Alerts"]
